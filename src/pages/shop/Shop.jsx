5e8af9e429192b9a432639c35b397e3bf59f96c6
Albert Lin <albert88919@yahoo.com.tw>
2023-04-12 14:16:12 +0800

    show cartbox when user pageYOffset > 200

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "./Product";
 import "./Shop.css";
 import { useSelector } from "react-redux";
+import { ShoppingCart } from "phosphor-react";
+import { useNavigate } from "react-router-dom";
 
 const Shop = () => {
   const products = useSelector((state) => state.products.PRODUCTS);
-  console.log(products);
+  const { cartItem } = useSelector((state) => state.products);
+  const quantity = cartItem.map((item) => item.quantity);
+  const totalQuantity = quantity.reduce((sum, item) => sum + item, 0);
+  const [cartBoxShow, setCartBoxShow] = useState(false);
+  const navigate = useNavigate();
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 200) {
+      setCartBoxShow(true);
+    } else {
+      setCartBoxShow(false);
+    }
+  });
+
   return (
     <div className="shop">
       <div className="shopTitle">
@@ -17,6 +32,14 @@ const Shop = () => {
             return <Product data={product} key={product.id} />;
           })}
         </div>
+      </div>
+      <div
+        className="cart-box"
+        onClick={() => navigate("/cart")}
+        style={{ bottom: cartBoxShow ? "80px" : "" }}
+      >
+        <ShoppingCart className="cartboxitem" weight="fill" fontSize={32} />
+        <span>{totalQuantity !== 0 && totalQuantity}</span>
       </div>
     </div>
   );
